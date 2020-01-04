0db7aa27e667e790b7e84d6f5220476696e4c662
handofragnaros <55743576+handofragnaros@users.noreply.github.com>
2020-01-03 19:02:28 -0500

    css

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import Header from "./Header.svelte";
+  import GlobalStyle from "./GlobalStyle.svelte";
 
   const fields = [
     {
@@ -61,47 +62,6 @@
 </script>
 
 <style type="text/scss">
-  :global(body) {
-    background-color: white;
-    padding: 0;
-  }
-  :global(input[type="range"]) {
-    -webkit-appearance: none;
-    width: 50%;
-    height: fit-content;
-    background: none;
-    outline: none;
-    border: none;
-    margin: auto;
-  }
-  :global(input[type="range"]:focus) {
-    outline: none;
-  }
-  :global(input[type="range"]::-webkit-slider-runnable-track) {
-    width: 100%;
-    height: 12.8px;
-    cursor: pointer;
-    animate: 0.2s;
-    box-shadow: 0px 0px 0px #000000, 0px 0px 0px #0d0d0d;
-    background: white;
-    border-radius: 25px;
-    border: 0px solid #000101;
-  }
-  :global(input[type="range"]::-webkit-slider-thumb) {
-    box-shadow: 0px 0px 0px #000000, 0px 0px 0px #0d0d0d;
-    border: 0px solid #000000;
-    height: 1.3em;
-    width: 1.2em;
-    border-radius: 1em;
-    background: gray;
-    cursor: pointer;
-    -webkit-appearance: none;
-    margin-top: -3.6px;
-  }
-  :global(input[type="range"]:focus::-webkit-slider-runnable-track) {
-    background: white;
-  }
-
   .wrapper {
     text-align: center;
     color: black;
